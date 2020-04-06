9dd0ba8ef6be8dc436943493dbadb93d6c926f88
shogo.egashira <shogo.11.7.17@gmail.com>
2020-04-06 07:21:23 +0000

    modify paginate name

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -24,7 +24,7 @@
             </tbody>
         </table>
     @endif
-    {{ $messages->links('pagination::bootstrap-4') }}
+    {{ $tasks->links('pagination::bootstrap-4') }}
     
     {!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!}
 
